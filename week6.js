6b22c4133dc70e10d85654371155cd397e5964b8
Natalie Childs <nata.faye@gmail.com>
2024-02-15 20:23:20 -0800

    cooking and formatting things

--- week6.js
+++ week6.js
@@ -1,85 +1,72 @@
-// Don't repeat yourself (DRY)
-// If you find that you're repeating yourself, a function will probably help
 
-// Output to be true if he should get the dog, false if not
-// OLD VERSION: You could make a parameter for each dog feature
-// function rafaelShouldGetDog(likesFetch, sheds, pottyTrained, likesSnuggles) {
-//     return pottyTrained && !sheds && (likesFetch || likesSnuggles)
-// }
-
-// OLD VERSION: You could make a separate object for each dog
-// const milo = {
-//     name: "Milo",
-//     likesFetch: true,
-//     sheds: true,
-//     pottyTrained: true,
-//     likesSnuggles: true
-// }
-// console.log("Should he get Milo?", rafaelShouldGetDog(milo))
-// const penny = {
-//     name: "Penny",
-//     likesFetch: true,
-//     sheds: false,
-//     pottyTrained: true,
-//     likesSnuggles: true
-// }
-// console.log("Should he get Penny?", rafaelShouldGetDog(penny))
-
-// Or the function could take a parameter that is an object with all the dog features as properties
-function rafaelShouldGetDog(dog) {
-    return dog.pottyTrained && !dog.sheds && (dog.likesFetch || dog.likesSnuggles)
+// saving to phone
+function myDad() {
+    1232332432
 }
 
-// Or the dogs could be stored in an array so we can easily add and remove dogs
-const shelterDogs = [
-    {
-        name: "Milo",
-        likesFetch: true,
-        sheds: true,
-        pottyTrained: true,
-        likesSnuggles: true
-    },
-    {
-        name: "Penny",
-        likesFetch: true,
-        sheds: false,
-        pottyTrained: true,
-        likesSnuggles: true
-    },
-    {
-        name: "Penny2",
-        likesFetch: true,
-        sheds: false,
-        pottyTrained: true,
-        likesSnuggles: true
-    },
-    {
-        name: "Penny3",
-        likesFetch: true,
-        sheds: false,
-        pottyTrained: true,
-        likesSnuggles: true
-    },
-    {
-        name: "Penny4",
-        likesFetch: true,
-        sheds: false,
-        pottyTrained: true,
-        likesSnuggles: true
+// call dad
+myDad()
+myDad()
+myDad()
+
+
+function cookInOven(food, temperature) {
+    return "Cooked " + food + " at " + temperature
+}
+
+function cookInMicrowave(food, seconds) {
+    let cookedFood = "Cooked " + food + " for " + seconds + " seconds"
+    return cookedFood
+}
+
+let cookedRice = cookInOven("rice", 200)
+let cookedChicken = cookInOven("chicken", 340)
+console.log(cookedRice + " and " + cookedChicken)
+
+// Does the exact same thing
+console.log(cookInOven("rice", 200) + " and " + cookInOven("chicken", 340))
+
+
+
+// Random function example
+function formatMoney(amount) {
+    return "$" + amount.toFixed(2)
+}
+
+const formattedPi = formatMoney(3.415)
+console.log(formattedPi)
+
+
+// Random function example
+function formatAmount(amount, noun) {
+    if(amount === 1) {
+        return amount + " " + noun
     }
-]
-
-// This loop will check each dog against Rafael's preferences, no matter how many are in the array
-for(let i = 0; i < shelterDogs.length; i++) {
-    console.log("Should he get " + shelterDogs[i].name + "?", rafaelShouldGetDog( shelterDogs[i] ))
+    return amount + " " + noun + "s"
 }
 
+let amountOfCats = 4
+console.log("You have " + formatAmount(amountOfCats, "cat"))
+
+// Don't do this to your poor cat! EVEN NUMBERS
+amountOfCats = 1
+console.log("You have " + formatAmount(amountOfCats, "cat"))
 
 
-// function cookInOven(food, temperature) { // let food = "rice"; let temperature = 200
-//     let cookedFood = "Cooked " + food + " at " + temperature
-//     return cookedFood
-// }
+// Random function example (very imporant)
+function hasCorrectAmountOfCats(amountOfCats) {
+    // if odd amount
+    if(amountOfCats === 1) {
+        alert("SATAN")
+    }
+    else if(amountOfCats % 2 === 1) {
+        alert("YOU HORRIBLE PERSON! Get another cat! Cats need friends!")
+    }
+    else {
+        alert("You're good. Carry on.")
+    }
+}
 
-// We can call functions wherever we want, and whatever it returns will be dumped right where we call it
-// console.log("Dinner is " + cookInOven("rice", 200) + " and " + cookInOven("chicken", 400))
+// hasCorrectAmountOfCats(3)
+
+// hasCorrectAmountOfCats(6)
