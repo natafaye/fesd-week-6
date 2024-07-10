0acb89277a04eba09334e8e3304cc3a99dd452b4
Natalie Childs <nata.faye@gmail.com>
2024-07-09 19:45:51 -0700

    dogs and fruits

--- week6.js
+++ week6.js
@@ -1,61 +1,54 @@
+let fruits = ["apple", "mango", "banana"]
+
+fruits.splice(1, 1)
 
 
-let runners = []
+let dogName
+let likesFetch
+let sheds
+let pottyTrained
+let likesSnuggles
 
-runners.push("Abigail")
-runners.push("Stacey")
-runners.push("James")
-runners.push("Maiya")
-
-// Logs out each runner's name and what place they got
-
-for(let i = 0; i < runners.length; i++) {
-    console.log(i + 1 + ": " + runners[i])
+// Making the function
+// Make the contact
+// parameter = fancy function variable
+function checkDog(dogName) { // in the background: let dogName = whatever was in the parenthesis
+    console.log("Should Rafael get " + dogName + "?")
+    let shouldGetDog = pottyTrained && !sheds && (likesFetch || likesSnuggles)
+    console.log(shouldGetDog)
 }
 
-let i = 0;
-while(i <= runners.length - 1) {
-    console.log(runners[i] + " you are in " + (i + 1) + " place") 
-}
-i++
+// Milo
+likesFetch = true
+sheds = true
+pottyTrained = true
+likesSnuggles = true
 
-
-
-// Creates the function
-function callDad() {
-    console.log("Calling 432432432")
-    console.log("Hey there!")
-}
-
-// call / invoke = to run the function
-callDad()
-
-
-// Create a function
-// parameter = input to the function
-function formatMoney(amount) {
-    // The function handles making the variable for you and setting it
-    // let amount = WHATEVER THEY PASSED IN
-    return "$" + amount.toFixed(2)
+let dog = {
+    likesFetch: true,
+    sheds: true,
+    pottyTrained: true,
+    likesSnuggles: true
 }
 
-// pass 2.7 into the amount parameter
-formatMoney(2.7)
+// Naming variables is hard but important
+let fruitList = ["pear", "apple"]
+let myFavoriteFruitIndex = 1
+console.log("My favorite fruit is " + fruitList[myFavoriteFruitIndex])
 
-// pass 5 into the amount parameter
-formatMoney(5)
+// parameter = input to the function = argument
+// pass in = give as a parameter to a function
+checkDog("Milo")
 
-// pass 2.58 into the amount parameter
-formatMoney(2.58)
+// Penny
+likesFetch = false
+sheds = false
+pottyTrained = true
+likesSnuggles = true
 
-let total = 34.57
-alert("Your total for the cart is " + formatMoney(total))
+// Calling the function
+// calling = using = running
+// tap the contact to call it
+checkDog("Penny")
 
-let giftCardBalance = 0.38
-alert("You have " + formatMoney(giftCardBalance) + " left over on your Gift Card")
-
-
-function toaster(bread) {
-    alert(bread) // tweeting out about the bread
-    return bread // giving the bread back to the person who started the toaster
-}
+console.log("All done!")
