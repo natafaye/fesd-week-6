b7ea788fcb002875277066aaf381deb10af7b822
Natalie Childs <nata.faye@gmail.com>
2024-12-18 18:38:06 -0800

    formatMoney and cookInOven

--- week6.js
+++ week6.js
@@ -1,54 +1,74 @@
-let fruits = ["apple", "mango", "banana"]
 
-fruits.splice(1, 1)
-
-
-let dogName
-let likesFetch
-let sheds
-let pottyTrained
-let likesSnuggles
-
-// Making the function
-// Make the contact
-// parameter = fancy function variable
-function checkDog(dogName) { // in the background: let dogName = whatever was in the parenthesis
-    console.log("Should Rafael get " + dogName + "?")
-    let shouldGetDog = pottyTrained && !sheds && (likesFetch || likesSnuggles)
-    console.log(shouldGetDog)
+// make (declare) a function
+function anyNameWeWant() {
+    // the code that will run when you call the function
+    alert("hello!")
 }
 
-// Milo
-likesFetch = true
-sheds = true
-pottyTrained = true
-likesSnuggles = true
+// call a function
+//anyNameWeWant()
+//anyNameWeWant()
+//anyNameWeWant()
 
-let dog = {
+// Two reasons we make functions
+// 1) is to make that code reusable
+// 2) is just to organize things - makes the code easier to read
+
+
+// input -> the thing I want formatted -> parameters
+function formatMoney(amount) { // amount is a parameter = a special variable that the function makes and assigns for you
+    // the function does this for us: let amount = 500
+    return "$" + amount.toFixed(2)
+    // return sends the value to wherever the function was called
+}
+
+let userMoney = 500
+let costOfThing = 20.4
+
+alert("You have " + formatMoney(userMoney) + " and the thing costs " + formatMoney(costOfThing))
+
+// We can pass in the number 4.7 into the amount parameter by doing this
+formatMoney(4.7)
+
+
+
+
+function cookInOven(food, temperature) { // food = "rice"; temperature = undefined
+    if(temperature === undefined) {
+        // Emergency exit out because something's not right
+        return "No temperature was given"
+    }
+    let cookedFood = "Cooked " + food + " at " + temperature
+    return cookedFood
+}
+
+// Calling a function will simplify to whatever that function returns
+// if the function returns nothing it will simplify to undefined
+// if the function returns something it will simplify to that thing
+let cookedRice = cookInOven("rice", 200)
+console.log(cookedRice)
+
+
+// Over the break stretch learning
+// 1) functions calling other functions - blackjack example
+// 2) arrays of objects
+
+
+// annoying
+let dogName = "Milo"
+let likesFetch = true
+let sheds = true
+let pottyTrained = true
+let likesSnuggles = true
+
+// much better!
+const dog = {
+    name: "Milo",
     likesFetch: true,
     sheds: true,
     pottyTrained: true,
     likesSnuggles: true
 }
 
-// Naming variables is hard but important
-let fruitList = ["pear", "apple"]
-let myFavoriteFruitIndex = 1
-console.log("My favorite fruit is " + fruitList[myFavoriteFruitIndex])
-
-// parameter = input to the function = argument
-// pass in = give as a parameter to a function
-checkDog("Milo")
-
-// Penny
-likesFetch = false
-sheds = false
-pottyTrained = true
-likesSnuggles = true
-
-// Calling the function
-// calling = using = running
-// tap the contact to call it
-checkDog("Penny")
-
-console.log("All done!")
+// = means "is"
+// : means "has"
