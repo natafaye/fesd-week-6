02ccb48a257963a4453aaa29d18c94dd13a02645
Natalie Childs <nata.faye@gmail.com>
2025-10-07 19:02:57 -0700

    hand values and things

--- week6.js
+++ week6.js
@@ -1,83 +1,100 @@
-let myFriends = ["billy", "bobby", "sally"]
 
-// fruits.splice(where to start, how many to delete, what to add)
+// let myColors = ["red", "orange", "green", "blue"]
+
+// myColors[0] = "fuschia"
+// myColors[1] = myColors[2] + "-blue"
+// let nextColor = myColors[4 - 1]
+// let lastColor = myColors[myColors.length - 1]
+
+// myColors.push("purple")
+
+// // Length of array
+// myColors.length
+
+// console.log(myColors) // ["fuschia", "green-blue", "green", "blue", "purple"]
+// console.log(nextColor) // "blue"
+
+// todolist = ["taxes", "laundry", "dishes"]
+// promptMessage = "What do you need to do?\n\n laundry,dishes"
+// newtodo = "taxes"
 
 
-// Saving a contact in your phone
-function nameOfTheFunction() {
-    console.log("hello!")
+// let todolist = ["laundry", "dishes"]
+
+// let promptMessage = "What do you need to do?\n\n"
+
+// let newtodo = prompt(promptMessage + todolist.join("\n"))
+// while(newtodo !== null) {
+//     todolist.unshift(newtodo)
+//     //promptMessage = "What do you need to do?\n\n" + todolist
+//     newtodo = prompt(promptMessage + todolist.join("\n"))
+// }
+
+// console.log(todolist)
+
+
+// The #1 Front End Bug
+// I clicked the button and it doesn't look like it did it
+// 3 Options of what happened:
+// Did the click not go through?
+// Did the change don't happen?
+// Did the UI not get updated with the change?
+
+
+
+// where you make the function doesn't really matter (generally)
+// shoes is a parameter (parameter = a variable thats an input to the function)
+// parameters allow us to customize what the function does
+function runARace(shoes) { // in the background: let shoes = whatever is passed in
+    // Code that will only happen when this function is "called"
+    for(let i = 0; i < 5; i++) {
+        console.log("Taking step " + (i + 1) + " with " + shoes)
+    }
+    return "You came in first place because of your " + shoes + "!"
 }
 
-// Calling the function
-nameOfTheFunction()
+let raceMessage = runARace("Nikes")
+console.log(raceMessage) // "You came in first place because of your Nikes!"
 
-
-// Rice cooker
-// parameter = special variable that's set up by the function
-function cookInRiceCooker(cupsOfRice) { // let cupsOfRice = WHATEVER IS PASSED IN
-    // pretend that you cook it for 3 * cupsOfRice minutes
-    let minutesToCook = cupsOfRice * 3
-    console.log("Cooking for " + minutesToCook + " minutes")
-}
-
-cookInRiceCooker(4)
-
-cookInRiceCooker(8)
+let raceMessage2 = runARace("Adidas")
+console.log(raceMessage) // "You came in first place because of your Adidas!"
 
 
 
 
 
-function cookInOven(food, temperature) { // let food = 200; let temperature = undefined
-    // return // emergency exit out of the function
-    let cookedFood = "Cooked " + food + " at " + temperature
-    return cookedFood
+function cookInOven(food, temperature) {
+  let cookedFood = "Cooked " + food + " at " + temperature
+  return cookedFood // GOODBYE I'M OUTTA HERE
+  alert("THIS CODE WILL NEVER RUN")
 }
 
 let cookedRice = cookInOven("rice", 200)
-console.log("What's for dinner? " + cookedRice)
+let cookedChicken = cookInOven("chicken", 300)
+alert("Dinner is:\n" + cookedRice + "\n" + cookedChicken)
 
-// console.log and alert show something to a human (user, developer)
-// Posting a picture on social media
-function toastBread(bread) {
-    alert("toasted " + bread) // Posting a picture of the toast on social media
 
-    return "toasted " + bread // actually handing you the toasted bread
+// a function call (like cookInOven("rice", 200) ) 
+// simplifies to whatever that function returned (like "Cooked rice at 200")
+
+
+// console.log and alert talk to the user
+// return talks to another part of the code
+
+
+
+function getHandValue(hand) { // let hand = [3, 5, 10]
+    let total = 0 // 18
+    for (let i = 0; i < hand.length; i++) { // i = 3
+        total += hand[i]
+    }
+    return total
 }
 
-// return gives a value to another part of the code
+let myHand = [3, 5, 10]
+let dealerHand = [1, 10, 10]
 
+let myHandValue = getHandValue(myHand) // WAIT
+let dealerHandValue = getHandValue(dealerHand)
 
-
-
-// Takes a number amount and returns a pretty string version
-// with a dollar sign in front and two decimal places
-function formatAsMoney(amount) {
-    return "$" + amount.toFixed(2)
-}
-
-// Making this a function had a few benefits:
-// * easier to change
-// * easier to read
-// * reduced repitition
-
-function checkOut(costOfItem1, costOfItem2) {
-    let total = costOfItem1 + costOfItem2
-    let taxRate = 0.08265
-    let salesTax = total * taxRate
-
-    console.log("Your pre-tax total is " + formatAsMoney(total))
-
-    console.log("You pay " + formatAsMoney(salesTax) + " in sales tax")
-
-    console.log("Your full total is " + formatAsMoney(total + salesTax))
-}
-
-checkOut(3, 4.55)
-
-checkOut(50.42, 1.03)
-
-
-
-// functions calling other functions (parameters and returns)
-// arrays of objects
+console.log("Mine: " + myHandValue + ", Dealer: " + dealerHandValue)
